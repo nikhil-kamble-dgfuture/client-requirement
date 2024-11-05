1f0f586a5df4ab51b42da879c1bf1cf9d50219be
Nikhil Kamble <nkamble@dgfuturetech.lan>
2024-11-05 11:19:16 +0530

    footer update

--- src/components/ClientQuoteRequirement.jsx
+++ src/components/ClientQuoteRequirement.jsx
@@ -840,7 +840,7 @@ const DubbingRequirements = () => {
       </div>
 
       <button type="submit" className="mt-6 bg-blue-600 text-white px-4 py-2 rounded">
-        Generate Quote
+        Submit
       </button>
     </div>
   );
